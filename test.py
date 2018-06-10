6c0f10a74299bc721cab06982bf32525bebf6e19
Kyle Turpin <gooba42@gmail.com>
2018-06-10 17:59:44 -0500

    test had been misconfigured for new hero methods

--- test.py
+++ test.py
@@ -2,12 +2,12 @@
 from hero import hero
 from rpgdice import gameDie
 
-heroName = input("What is your hero's name?") or "Smacky"
-heroBest = input("What is your hero's best skill?") or "Smacking"
-heroMid = input("What is your hero's second best skill?") or "Cooking"
-heroWorst = input("What is your hero's worst skill?") or "Hiding"
+heroName = input("What is your hero's name? ") or "Smacky"
+heroBest = input("What is your hero's best skill? ") or "Smacking"
+heroMid = input("What is your hero's second best skill? ") or "Cooking"
+heroWorst = input("What is your hero's worst skill? ") or "Hiding"
 heroLevel = 0
-myHero = hero(heroName,heroLevel,heroBest,heroMid,heroWorst)
+myHero = hero(heroName,heroBest,heroMid,heroWorst,heroLevel)
 
 worstDie = gameDie(myHero.skills[myHero.worst])
 
