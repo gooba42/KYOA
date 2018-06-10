8257b002a21c28b34f879fe1c2b4f89e76bbad73
Kyle Turpin <gooba42@gmail.com>
2018-06-10 15:38:37 -0500

    Fixed hero.py to update skills and levels correctly Added test.py for running a test instance of whatever's fresh

--- hero.py
+++ hero.py
@@ -1,19 +1,20 @@
 class hero:
-    def __init__(self, name, level, best, middling, worst):
+    """This creates a hero suitable for a kid fantasy game.
+    They have 3 skills: Best, Second Best and Worst
+    These are represented by different gaming dice with higher
+    skills represented by dice with more sides for greater chance
+    of a higher-than-threshold roll."""
+    def __init__(self, name, best, middling, worst,level=0):
         self.name = name
         self.best = best
         self.middling = middling
         self.worst = worst
-        self.bestDie = 0
-        self.mDie = 0
-        self.wDie = 0
+        self.bestDie = 1
+        self.mDie = 1
+        self.wDie = 1
         self.level = level
-        self.bestDie, self.mDie, self.wDie = self.levelChange(level)
-        self.skills = {
-            self.best: self.bestDie,
-            self.middling: self.mDie,
-            self.worst: self.wDie
-        }
+        self.setLevel(level)
+        self.updateSkills()
 
     def __call__(self):
         stats = [
@@ -22,23 +23,47 @@ class hero:
         ]
         return stats
 
-    def levelChange(self,newLevel):
+    def setLevel(self,newLevel):
+        """This should update the dice according to the new level applied to
+        the hero."""
         self.level = newLevel
-        if(newLevel == 1):
-            self.bestDie = 10
-            self.mDie = 8
-            self.wDie = 6
-        elif(newLevel == 2):
-            self.bestDie = 12
-            self.mDie = 10
-            self.wDie = 6
-        elif(newLevel == 3):
-            self.bestDie = 20
-            self.mDie = 12
-            self.wDie = 8
-        else:
-            self.bestDie = 8
-            self.mDie = 6
+        if(self.level == 0):
+            #Regular Person Stats
             self.wDie = 4
-        return
+            self.mDie = 6
+            self.bestDie = 8
+
+        elif(self.level == 1):
+            #Skilled Person Stats
+            self.wDie = 6
+            self.mDie = 8
+            self.bestDie = 10
+
+        elif(self.level == 2):
+            #Amazing person stats
+            self.wDie = 8
+            self.mDie = 10
+            self.bestDie = 12
+
+        elif(self.level == 3):
+            #Superhuman stats
+            self.wDie = 10
+            self.mDie = 12
+            self.bestDie = 20
+
+        else:
+            #If all else fails,default to minimal but legal stats
+            self.wDie = 1
+            self.mDie = 1
+            self.bestDie = 1
+        self.updateSkills()
+
+    def updateSkills(self):
+        """We update the skills block for accurate reporting with the
+        __call__ method"""
+        self.skills = {
+                self.best: self.bestDie,
+                self.middling: self.mDie,
+                self.worst: self.wDie
+                }
 
---
+++ test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from hero import hero
+from rpgdice import gameDie
+
+heroName = input("What is your hero's name?") or "Smacky"
+heroBest = input("What is your hero's best skill?") or "Smacking"
+heroMid = input("What is your hero's second best skill?") or "Cooking"
+heroWorst = input("What is your hero's worst skill?") or "Hiding"
+heroLevel = 0
+myHero = hero(heroName,heroLevel,heroBest,heroMid,heroWorst)
+
+worstDie = gameDie(myHero.skills[myHero.worst])
+
+print(myHero())
+print(myHero.worst+" rolled "+ str(worstDie.roll()))
+
+myHero.setLevel(3)
+
+worstDie = gameDie(myHero.skills[myHero.worst])
+print(myHero())
+print(myHero.worst+" rolled "+ str(worstDie.roll()))
+
